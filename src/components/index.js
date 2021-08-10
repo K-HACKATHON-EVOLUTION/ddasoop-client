a3070db34e02d7a9636ae021a67fd30b44728a9e
Lynn <dazzlynnnn@gmail.com>
2021-08-10 22:42:23 +0900

    Update: index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import Image from "./Image";
 import Input from "./Input";
+import MyGroup from "./MyGroup";
+import SearchBar from "./SearchBar";
+import Group from "./Group";
 
-export { Image, Input };
+export { Image, Input, MyGroup, SearchBar, Group };
