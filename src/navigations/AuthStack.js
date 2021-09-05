02f472f4a76ac86be45ff63d3450927fce94c502
Haerin Lee <dazzlynnnn@gmail.com>
2021-09-05 18:32:12 +0900

    Update AuthStack screenOptions

--- src/navigations/AuthStack.js
+++ src/navigations/AuthStack.js
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react';
-import { ThemeContext } from 'styled-components/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Login, Signup } from '../screens';
+import React, { useContext } from "react";
+import { AntDesign } from "@expo/vector-icons";
+import { ThemeContext } from "styled-components/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Login, Signup } from "../screens";
 
 const Stack = createStackNavigator();
 
@@ -11,12 +12,34 @@ const AuthStack = () => {
     <Stack.Navigator
       initialRouteName="Login"
       screenOptions={{
-        headerTitleAlign: 'center',
-        headerTintColor: theme.headerTintColor,
-        cardStyle: { backgroundColor: theme.background }
+        cardStyle: { backgroundColor: theme.background },
+        headerStyle: {
+          height: 110,
+          backgroundColor: theme.background,
+        },
+        headerTitleStyle: {
+          color: theme.headerTintColor,
+          fontSize: 25,
+          fontWeight: "bold",
+        },
+        headerTitleAlign: "center",
+        headerBackTitleVisible: false,
+        headerBackImage: () => {
+          const style = {
+            marginRight: 5,
+            marginLeft: Platform.OS === "ios" ? 15 : 0,
+          };
+          return (
+            <AntDesign name="left" size={25} color={theme.headerTintColor} style={style} />
+          );
+        },
       }}
     >
-      <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen name="Signup" component={Signup} />
     </Stack.Navigator>
   );
