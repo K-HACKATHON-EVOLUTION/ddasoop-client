157348fa9a5b0626f2d6ca8696e254ca6a5e9f92
Haerin Lee <dazzlynnnn@gmail.com>
2021-09-05 18:36:55 +0900

    Add validateEmail, removeWhitespace functions to Input components

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,6 +3,7 @@ import styled from 'styled-components/native';
 import { Image, Input } from '../components';
 import { images } from '../utils/images';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { validateEmail, removeWhitespace } from '../utils/common';
 
 const Container = styled.View`
     flex: 1;
@@ -12,11 +13,31 @@ const Container = styled.View`
     padding: 20px;
 `;
 
+const ErrorText = styled.Text`
+    align-items: flex-start;
+    width: 100%;
+    height: 20px;
+    margin-bottom: 10px;
+    line-height: 20px;
+    color: ${({ theme }) => theme.errorText};
+`;
+
 const Login = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
     const passwordRef = useRef();
 
+    const _handleEmailChange = email => {
+        const changedEmail = removeWhitespace(email);
+        setEmail(changedEmail);
+        setErrorMessage(
+            validateEmail(changedEmail) ? '' : '올바르지 않은 이메일 형식입니다.'
+        );
+    };
+    const _handlePasswordChange = password => {
+        setPassword(removeWhitespace(password));
+    };
     return (
         <KeyboardAwareScrollView
             contentContainerStyle={{ flex: 1 }}
@@ -27,7 +48,7 @@ const Login = ({ navigation }) => {
                 <Input
                     label="Email"
                     value={email}
-                    onChangeText={text => setEmail(text)}
+                    onChangeText={_handleEmailChange}
                     onSubmitEditing={() => passwordRef.current.focus()}
                     placeholder="Email"
                     returnKeyType="next"
@@ -36,12 +57,13 @@ const Login = ({ navigation }) => {
                     ref={passwordRef}
                     label="Password"
                     value={password}
-                    onChangeText={text => setPassword(text)}
                     onSubmitEditing={() => { }}
+                    onChangeText={_handlePasswordChange}
                     placeholder="Password"
                     returnKeyType="done"
                     isPassword
                 />
+                <ErrorText>{errorMessage}</ErrorText>
             </Container>
         </KeyboardAwareScrollView>
     );
