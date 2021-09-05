1d78a276cad65d4c4d3808fe8f24e4bfe2e10edb
Haerin Lee <dazzlynnnn@gmail.com>
2021-09-05 17:40:32 +0900

    Add KeyboardAwareScrollView
    react-native-keyboard-aware-scroll-view 라이브러리에서 제공하는 KeyboardAwareScrollView 컴포넌트를 로그인 화면에 적용하면, 입력 도중 다른 영역을 터치했을 때 키보드가 사라질 뿐만 아니라 포커스를 얻은 Input 컴포넌트의 위치에 맞춰 스크롤이 이동한다.

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,15 +1,15 @@
 import React, { useState, useRef } from 'react';
 import styled from 'styled-components/native';
-import { Text, Button } from 'react-native';
 import { Image, Input } from '../components';
 import { images } from '../utils/images';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: ${({ theme }) => theme.background};
-  padding: 20px;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: ${({ theme }) => theme.background};
+    padding: 20px;
 `;
 
 const Login = ({ navigation }) => {
@@ -18,27 +18,32 @@ const Login = ({ navigation }) => {
     const passwordRef = useRef();
 
     return (
-        <Container>
-            <Image url={images.logo} imageStyle={{ borderRadius: 8 }} />
-            <Input
-                label="Email"
-                value={email}
-                onChangeText={text => setEmail(text)}
-                onSubmitEditing={() => passwordRef.current.focus()}
-                placeholder="Email"
-                returnKeyType="next"
-            />
-            <Input
-                ref={passwordRef}
-                label="Password"
-                value={password}
-                onChangeText={text => setPassword(text)}
-                onSubmitEditing={() => {}}
-                placeholder="Password"
-                returnKeyType="done"
-                isPassword
-            />
-        </Container>
+        <KeyboardAwareScrollView
+            contentContainerStyle={{ flex: 1 }}
+            extraScrollHeight={20}
+        >
+            <Container>
+                <Image url={images.logo} imageStyle={{ borderRadius: 8 }} />
+                <Input
+                    label="Email"
+                    value={email}
+                    onChangeText={text => setEmail(text)}
+                    onSubmitEditing={() => passwordRef.current.focus()}
+                    placeholder="Email"
+                    returnKeyType="next"
+                />
+                <Input
+                    ref={passwordRef}
+                    label="Password"
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                    onSubmitEditing={() => { }}
+                    placeholder="Password"
+                    returnKeyType="done"
+                    isPassword
+                />
+            </Container>
+        </KeyboardAwareScrollView>
     );
 };
 
