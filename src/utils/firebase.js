58bb0f894015b02a7051d210e4b1ea38d2acee48
Haerin Lee <dazzlynnnn@gmail.com>
2021-09-07 15:06:48 +0900

    Add uploadImage
    사용자가 선택한 사진을 storage에 업로드하고 그 url을 user 객체의 photoURL로 설정
    현재 인증된 사용자의 정보를 담은 currentUser의 uid를 이용해 사진이 저장될 주소를 구분하도록 함. 이렇게 사용자의 uid를 이용해서 파일이 저장되는 주소를 지정하면 규칙 수정을 통해 파일의 접근 권한을 설정하기 쉬울 뿐 아니라, 해당 사용자의 사진을 쉽게 찾을 수 있음.

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -10,7 +10,29 @@ export const login = async ({ email, password }) => {
     return user;
 };
 
-export const db = app.firestore();const uploadImage = async uri => {
+const uploadImage = async uri => {
+    const blob = await new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            resolve(xhr.response);
+        };
+        xhr.onerror = function (e) {
+            reject(new TypeError('Network request failed'));
+        };
+        xhr.responseType = 'blob';
+        xhr.open('GET', uri, true);
+        xhr.send(null);
+    });
+
+    const user = Auth.currentUser;
+    const ref = firebase.storage().ref(`/profile/${user.uid}/photo.png`);
+    const snapshot = await ref.put(blob, { contentType: 'image/png' });
+    
+    blob.close();
+    return await snapshot.ref.getDownloadURL();
+};
+
+export const signup = async ({ email, password, name, photoUrl }) => {
     const { user } = await Auth.createUserWithEmailAndPassword(email, password);
     const storageUrl = photoUrl.startsWith('https')
         ? photoUrl
