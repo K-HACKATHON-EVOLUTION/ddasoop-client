c4f6dc021a254d0f496395716c54639c20a9d27a
Haerin Lee <dazzlynnnn@gmail.com>
2021-09-12 13:59:35 +0900

    Fix screen layouts

--- src/screens/Tree.js
+++ src/screens/Tree.js
@@ -1,23 +1,97 @@
-import React from 'react';
-import styled from 'styled-components/native';
+import React, { useState } from "react";
+import styled from "styled-components/native";
+import { ScrollView, Image, View } from "react-native";
+import { theme } from "../theme";
+import { ThemeProvider } from "styled-components";
+import { Badge, Record } from "../components";
 
 const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  padding: 30px;
+`;
+
+const Wrapper = styled.View`
+  flex-direction: column;
+  margin-bottom: 30px;
 `;
 
 const StyledText = styled.Text`
-  font-size: 30px;
+  font-weight: 500;
+  font-size: 20px;
   margin-bottom: 10px;
 `;
 
-const Tree = () => {
-    return (
+const logs = [
+  {
+    logIdx: 1,
+    logDate: "2021-08-08",
+    dayOfWeek: 4,
+    hours: 1,
+    minutes: 3,
+    carbon: 1.32
+  },
+  {
+    logIdx: 2,
+    logDate: "2021-08-08",
+    dayOfWeek: 4,
+    hours: 1,
+    minutes: 3,
+    carbon: 1.36
+  },
+];
+
+
+const Tree = ({ navigation }) => {
+  const _onPress = (log) => {
+    navigation.navigate("Log", {
+      carbon: log.carbon,
+    });
+  };
+  return (
+    <ScrollView>
+      <ThemeProvider theme={theme}>
         <Container>
-            <StyledText>Tree</StyledText>
+          <Wrapper>
+            <StyledText>ㅇㅇ 님의 나무</StyledText>
+            <View style={{alignItems: 'center'}} >
+              <Image
+                source={require('../../assets/maintree.png')}
+                style={{
+                  resizeMode: "contain",
+                  width: 120,
+                  height: 120
+                }}
+              />
+            </View>
+          </Wrapper>
+          <Wrapper>
+            <StyledText>획득한 뱃지</StyledText>
+            <View style={{ display: 'flex', flexDirection: 'row'}}>
+              <Badge/>
+              <Badge/>
+              <Badge/>
+            </View>
+          </Wrapper>
+          <Wrapper>
+            <StyledText>기록</StyledText>
+            {logs.map((log) => (
+            <Record
+              key={log.logIdx}
+              date={log.logDate}
+              day={log.dayOfWeek}
+              hours={log.hours}
+              minutes={log.minutes}
+              carbon={log.carbon}
+              onPress={() => _onPress(log)}
+            />
+          ))}
+          </Wrapper>
         </Container>
-    );
+      </ThemeProvider>
+    </ScrollView>
+  );
 };
 
 export default Tree;
