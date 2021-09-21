b29f83a1bac2c7227df8b1ca22bd660b92cef67c
Haerin Lee <dazzlynnnn@gmail.com>
2021-09-21 20:51:25 +0900

    Add displayName, totalCarbon to user context

--- src/contexts/User.js
+++ src/contexts/User.js
@@ -4,15 +4,18 @@ const UserContext = createContext({
     user: {
         email: null,
         uid: null,
+        displayName: null,
+        totalCarbon: null,
         group: null,
+
     },
     dispatch: () => {},
 });
 
 const UserProvider = ({ children }) => {
     const [user, setUser] = useState({});
-    const dispatch = ({ email, uid }) => {
-        setUser({ email, uid, group: null });
+    const dispatch = ({ email, uid, totalCarbon, displayName }) => {
+        setUser({ email, uid, totalCarbon, displayName, group: null });
     };
     const value = { user, dispatch };
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
