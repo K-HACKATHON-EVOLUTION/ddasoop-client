06b163eb76f0deb6eafdef1cc5e191a96547f2ac
Haerin Lee <dazzlynnnn@gmail.com>
2021-09-24 21:59:20 +0900

    Add group related APIs

--- src/screens/GroupList.js
+++ src/screens/GroupList.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import styled from "styled-components/native";
 import { ScrollView } from "react-native";
 import { theme } from "../theme";
 import { ThemeProvider } from "styled-components";
 import { MyGroup, SearchBar, Group } from "../components";
+import { UserContext } from '../contexts';
 import axios from "axios";
-// import https from "https";
 
 const Container = styled.View`
   flex-direction: column;
@@ -18,39 +18,46 @@ const StyledText = styled.Text`
   font-size: 20px;
 `;
 
-const groups = [
-  {
-    forestIdx: 1,
-    forestName: "test",
-    leader: 1,
-    size: 3,
-    forestImg: null,
-    deleteFlag: false,
-  },
-  {
-    forestIdx: 2,
-    forestName: "test2",
-    leader: 1,
-    size: 3,
-    forestImg: null,
-    deleteFlag: false,
-  },
-  {
-    forestIdx: 3,
-    forestName: "test3",
-    leader: 1,
-    size: 3,
-    forestImg: null,
-    deleteFlag: false,
-  },
-];
-
 const GroupList = ({ navigation }) => {
+  const { user } = useContext(UserContext);
   const [search, setSearch] = useState("");
-  
+  const [groups, setGroup] = useState([]);
+  const [mygroup, setMygroup] = useState({});
+
+  const getGroups = async () => {
+    try{
+        const {data} = await axios.get(`http://13.125.127.125:8080/api/forests`);
+        setGroup(data);
+        return;
+    }
+    catch(e){
+        console.log("getGroup error");
+    }
+  }
+
+  const getMygroup = async (uid) => {
+    try{
+        const {data} = await axios.get(`http://13.125.127.125:8080/api/users/${uid}/forest`);
+        setMygroup(data);
+        return;
+    }
+    catch(e){
+        console.log("getMygroup error");
+    }
+  }
+
+  useEffect(() => {
+    getMygroup(user?.uid);
+    getGroups();
+  }, [groups]);
+
   const _onPress = (group) => {
     navigation.navigate("Group", {
+      index: group.forestIdx,
+      leader: group.leader,
       name: group.forestName,
+      size: group.size,
+      img: group.forestImg
     });
   };
 
@@ -59,7 +66,7 @@ const GroupList = ({ navigation }) => {
       <ThemeProvider theme={theme}>
         <Container>
           <StyledText>MY 숲</StyledText>
-          <MyGroup />
+          <MyGroup name={mygroup.forestName} img={mygroup.forestImg}/>
           <StyledText>숲 둘러보기</StyledText>
           <SearchBar
             value={search}
