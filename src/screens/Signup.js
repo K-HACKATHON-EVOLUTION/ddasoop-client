7ff53e21ea3c17625a5c963d1336bee6e7d1e506
Haerin Lee <dazzlynnnn@gmail.com>
2021-09-25 23:55:39 +0900

    Style format

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,12 +1,13 @@
-import React, { useContext, useState, useRef, useEffect } from 'react';
-import styled from 'styled-components/native';
-import { Image, Input, Button } from '../components';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { validateEmail, removeWhitespace } from '../utils/common';
-import { images } from '../utils/images';
-import { signup } from '../utils/firebase';
-import { Alert } from 'react-native';
-import { ProgressContext, UserContext } from '../contexts';
+import React, { useContext, useState, useRef, useEffect } from "react";
+import styled from "styled-components/native";
+import { Image, Input, Button } from "../components";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { validateEmail, removeWhitespace } from "../utils/common";
+import { images } from "../utils/images";
+import { signup } from "../utils/firebase";
+import { Alert } from "react-native";
+import { ProgressContext, UserContext } from "../contexts";
+import axios from "axios";
 
 const Container = styled.View`
     justify-content: center;
@@ -28,11 +29,11 @@ const Signup = () => {
     const { dispatch } = useContext(UserContext);
     const { spinner } = useContext(ProgressContext);
     const [photoUrl, setPhotoUrl] = useState(images.photo);
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordConfirm, setPasswordConfirm] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
     const [disabled, setDisabled] = useState(true);
 
     const emailRef = useRef();
@@ -43,23 +44,46 @@ const Signup = () => {
 
     useEffect(() => {
         if (didMountRef.current) {
-            let _errorMessage = '';
+            let _errorMessage = "";
             if (!name) {
-                _errorMessage = '이름을 입력해 주세요.';
+                _errorMessage = "이름을 입력해 주세요.";
             } else if (!validateEmail(email)) {
-                _errorMessage = '이메일 형식을 확인해 주세요.';
+                _errorMessage = "이메일 형식을 확인해 주세요.";
             } else if (password.length < 6) {
-                _errorMessage = '비밀번호는 최소 6자 이상이어야 합니다.';
+                _errorMessage = "비밀번호는 최소 6자 이상이어야 합니다.";
             } else if (password !== passwordConfirm) {
-                _errorMessage = '비밀번호가 다릅니다.';
+                _errorMessage = "비밀번호가 다릅니다.";
             } else {
-                _errorMessage = '';
+                _errorMessage = "";
             }
             setErrorMessage(_errorMessage);
-        } else { 
+        } else {
             didMountRef.current = true;
         }
     }, [name, email, password, passwordConfirm]);
+
+    const makeUser = async (uid, name) => {
+        try {
+        await axios.post(
+            "http://13.125.127.125:8080/api/users/",
+            {
+                "userIdx": uid,
+                "userName": name,
+            });
+        } catch (e) {
+            console.log('makeUser error');
+        }
+    };
+
+    const getUser = async (uid) => {
+        try{
+            const {data: user} = await axios.get(`http://13.125.127.125:8080/api/users/${uid}`);
+            return user;
+        }
+        catch(e){
+            console.log("getUser error");
+        }
+    }
 
     useEffect(() => {
         setDisabled(
@@ -71,9 +95,14 @@ const Signup = () => {
         try {
             spinner.start();
             const user = await signup({ email, password, name, photoUrl });
+            await makeUser(user.uid, user.displayName);
+            const gotuser = await getUser(user.uid);
+            user.userName = gotuser.userName;
+            user.treeImg = gotuser.treeImg;
+            user.totalCarbon = gotuser.totalCarbon;
             dispatch(user);
         } catch (e) {
-            Alert.alert('Signup Error', e.message);
+            Alert.alert("Signup Error", e.message);
         } finally {
             spinner.stop();
         }
@@ -81,61 +110,61 @@ const Signup = () => {
 
     return (
         <KeyboardAwareScrollView extraScrollHeight={20}>
-            <Container>
-                <Image
-                    rounded
-                    url={photoUrl}
-                    showButton
-                    onChangeImage={url => setPhotoUrl(url)}
-                />
-                <Input
-                    label="Name"
-                    value={name}
-                    onChangeText={text => setName(text)}
-                    onSubmitEditing={() => {
-                        setName(name.trim());
-                        emailRef.current.focus();
-                    }}
-                    onBlur={() => setName(name.trim())}
-                    placeholder="Name"
-                    returnKeyType="next"
-                />
-                <Input
-                    ref={emailRef}
-                    label="Email"
-                    value={email}
-                    onChangeText={text => setEmail(removeWhitespace(text))}
-                    onSubmitEditing={() => passwordRef.current.focus()}
-                    placeholder="Email"
-                    returnKeyType="next"
-                />
-                <Input
-                    ref={passwordRef}
-                    label="Password"
-                    value={password}
-                    onChangeText={text => setPassword(removeWhitespace(text))}
-                    onSubmitEditing={() => passwordConfirmRef.current.focus()}
-                    placeholder="Password"
-                    returnKeyType="next"
-                    isPassword
-                />
-                <Input
-                    ref={passwordConfirmRef}
-                    label="Password Confirm"
-                    value={passwordConfirm}
-                    onChangeText={text => setPasswordConfirm(removeWhitespace(text))}
-                    onSubmitEditing={_handleSignupButtonPress}
-                    placeholder="Password Confirm"
-                    returnKeyType="done"
-                    isPassword
-                />
-                <ErrorText>{errorMessage}</ErrorText>
-                <Button
-                    title="완료"
-                    onPress={_handleSignupButtonPress}
-                    disabled={disabled}
-                />
-            </Container>
+        <Container>
+            <Image
+            rounded
+            url={photoUrl}
+            showButton
+            onChangeImage={(url) => setPhotoUrl(url)}
+            />
+            <Input
+            label="Name"
+            value={name}
+            onChangeText={(text) => setName(text)}
+            onSubmitEditing={() => {
+                setName(name.trim());
+                emailRef.current.focus();
+            }}
+            onBlur={() => setName(name.trim())}
+            placeholder="Name"
+            returnKeyType="next"
+            />
+            <Input
+            ref={emailRef}
+            label="Email"
+            value={email}
+            onChangeText={(text) => setEmail(removeWhitespace(text))}
+            onSubmitEditing={() => passwordRef.current.focus()}
+            placeholder="Email"
+            returnKeyType="next"
+            />
+            <Input
+            ref={passwordRef}
+            label="Password"
+            value={password}
+            onChangeText={(text) => setPassword(removeWhitespace(text))}
+            onSubmitEditing={() => passwordConfirmRef.current.focus()}
+            placeholder="Password"
+            returnKeyType="next"
+            isPassword
+            />
+            <Input
+            ref={passwordConfirmRef}
+            label="Password Confirm"
+            value={passwordConfirm}
+            onChangeText={(text) => setPasswordConfirm(removeWhitespace(text))}
+            onSubmitEditing={_handleSignupButtonPress}
+            placeholder="Password Confirm"
+            returnKeyType="done"
+            isPassword
+            />
+            <ErrorText>{errorMessage}</ErrorText>
+            <Button
+            title="완료"
+            onPress={_handleSignupButtonPress}
+            disabled={disabled}
+            />
+        </Container>
         </KeyboardAwareScrollView>
     );
 };
