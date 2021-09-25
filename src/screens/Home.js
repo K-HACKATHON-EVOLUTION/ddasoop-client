b96995a5c9369019b44b7b0299ac71b934531207
Haerin Lee <dazzlynnnn@gmail.com>
2021-09-25 23:54:35 +0900

    Style format

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,85 +1,143 @@
-import React, { useContext, useLayoutEffect } from 'react';
-import styled from 'styled-components/native';
-import { View, Button, Image, ScrollView, TouchableOpacity } from 'react-native';
-import { AntDesign, Feather } from '@expo/vector-icons'; 
+import React, { useContext, useLayoutEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import { AntDesign, Feather } from "@expo/vector-icons";
 import { UserContext } from "../contexts";
-import axios from 'axios';
-
-const Container = styled.View`
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-`;
-const TitleText = styled.Text`
-  font-size: 25px;
-  margin-bottom: 5px;
-  font-weight: bold;
-  color: #848484;
-`;
-const SubText = styled.Text`
-  font-size: 15px;
-  color: gray;
-  margin-bottom: 80px;
-`;
 
 const Home = ({ navigation }) => {
   const { user } = useContext(UserContext);
-
   useLayoutEffect(() => {
     navigation.setOptions({
       headerBackTitleVisible: false,
-      headerTintColor: '#848484',
+      headerTintColor: "#848484",
       headerLeft: ({ tintColor }) => (
         <AntDesign
           name="setting"
           size={25}
           color={tintColor}
           style={{ marginLeft: 15 }}
-          onPress={()=>navigation.navigate('Setting')}
+          onPress={() => navigation.navigate("Setting")}
         />
       ),
       headerRight: ({ tintColor }) => (
         <AntDesign
-        name="bells"
-        size={25}
-        color={tintColor}
-        style={{ marginRight: 15 }}
-        onPress={()=>navigation.navigate('Alert')}
-      />
-      )
-    })
-  }, [])
-    return (
-      <Container>
-        <ScrollView style={{width:'100%'}}>
-          <Container>
-              <TouchableOpacity onPress={() => navigation.navigate('Tree')}>
-                <Image source={require('../../assets/maintree.png')} style={{ marginTop: 180, marginBottom: 80 }}/>
-              </TouchableOpacity>
-              <TitleText>{user?.totalCarbon}kg</TitleText>
-              <SubText>총 탄소저감량</SubText>
-              <Feather name="chevrons-down" color='#848484' size={25} style={{marginBottom: 10}} />
-              <Feather name="chevrons-down" color='#848484' size={25} style={{marginBottom: 10}} />
-              <Feather name="chevrons-down" color='#848484' size={25} style={{marginBottom: 10}} />
-              <Image source={require('../../assets/graph.png')} style={{ marginTop: 180, marginBottom: 60 }}/>
-              <Image source={require('../../assets/calendar.png')} style={{ marginBottom: 20 }}/>
-              
-          </Container>
-        </ScrollView>
-        <View style={{flexDirection: 'row', width:'100%', justifyContent: 'center',
-          height: 85, borderTopWidth:'1', borderTopColor: 'lightgray'}}>
-          <TouchableOpacity onPress={() => navigation.navigate('CourseMain')}>
-            <Image source={require('../../assets/course.png')} style={{marginTop:18, marginRight:50}}/>
-          </TouchableOpacity> 
-          <TouchableOpacity onPress={() => navigation.navigate('CourseMain')}>
-            <Image source={require('../../assets/start.png')} style={{marginTop:10}}/>
-          </TouchableOpacity>           
-          <TouchableOpacity onPress={() => navigation.navigate('GroupList')}>
-            <Image source={require('../../assets/group.png')} style={{marginTop:18, marginLeft:50}}/>
+          name="bells"
+          size={25}
+          color={tintColor}
+          style={{ marginRight: 15 }}
+          onPress={() => navigation.navigate("Alert")}
+        />
+      ),
+    });
+  }, []);
+  return (
+    <View style={styles.container}>
+      <ScrollView style={{ width: "100%" }}>
+        <View style={styles.container}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Tree")}
+            style={styles.touchable}
+          >
+            <Image source={{ uri: user?.treeImg }} style={styles.tree} />
           </TouchableOpacity>
+          <Text style={styles.title}>{user?.totalCarbon}kg</Text>
+          <Text style={styles.subtitle}>총 탄소저감량</Text>
+          <Feather
+            name="chevrons-down"
+            color="#848484"
+            size={25}
+            style={{ marginBottom: 10 }}
+          />
+          <Feather
+            name="chevrons-down"
+            color="#848484"
+            size={25}
+            style={{ marginBottom: 10 }}
+          />
+          <Feather
+            name="chevrons-down"
+            color="#848484"
+            size={25}
+            style={{ marginBottom: 10 }}
+          />
+          <Image
+            source={require("../../assets/graph.png")}
+            style={{ marginTop: 180, marginBottom: 60 }}
+          />
+          <Image
+            source={require("../../assets/calendar.png")}
+            style={{ marginBottom: 20 }}
+          />
         </View>
-      </Container>
-    );
+      </ScrollView>
+      <View style={styles.bar}>
+        <TouchableOpacity onPress={() => navigation.navigate("CourseMain")}>
+          <Image
+            source={require("../../assets/course.png")}
+            style={{ marginTop: 18, marginRight: 50 }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("CourseMain")}>
+          <Image
+            source={require("../../assets/start.png")}
+            style={{ marginTop: 10 }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("GroupList")}>
+          <Image
+            source={require("../../assets/group.png")}
+            style={{ marginTop: 18, marginLeft: 50 }}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 25,
+    marginBottom: 5,
+    fontWeight: "bold",
+    color: "#848484",
+  },
+  subtitle: {
+    fontSize: 15,
+    marginBottom: 100,
+    color: "gray",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bar: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "center",
+    height: 85,
+    borderTopWidth: 1,
+    borderTopColor: "lightgray",
+  },
+  touchable: {
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 170,
+    height: 200,
+    width: 200,
+  },
+  tree: {
+    backgroundColor: "#ffffff",
+    resizeMode: "contain",
+    width: 200,
+    height: 200,
+  },
+});
 
 export default Home;
