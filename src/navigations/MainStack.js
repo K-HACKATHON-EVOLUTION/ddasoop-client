9ac20215a67bb667dbc6e9890894b866681d34b9
Haerin Lee <dazzlynnnn@gmail.com>
2021-10-01 18:14:37 +0900

    Delete BadgeList screen

--- src/navigations/MainStack.js
+++ src/navigations/MainStack.js
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Home, Tree, GroupList, Group, Alert, Setting, Log, CourseMain, BadgeList } from "../screens";
 import { AntDesign } from "@expo/vector-icons";
 import { Platform } from "react-native";
 import { UserContext } from "../contexts";
@@ -65,9 +64,6 @@ const MainStack = () => {
         options={{ headerTitle: "나의 나무" }}
       />
       <Stack.Screen
-        name="BadgeList"
-        component={BadgeList}
-        options={{ headerTitle: "획득한 뱃지" }}
       />
       <Stack.Screen
         name="Log"
