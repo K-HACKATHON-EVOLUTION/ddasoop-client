d6da052fecbefe90eeca76e4e5d754a5ddbc3f01
Haerin Lee <dazzlynnnn@gmail.com>
2021-10-10 23:09:15 +0900

    Add AlertBar component to Alert screen

--- src/screens/Alert.js
+++ src/screens/Alert.js
@@ -1,21 +1,16 @@
 import React from 'react';
+import { AlertBar } from "../components";
 import styled from 'styled-components/native';
 
 const Container = styled.View`
   flex: 1;
-  justify-content: center;
   align-items: center;
-`;
-
-const StyledText = styled.Text`
-  font-size: 20px;
-  color: gray;
 `;
 
 const Alert = () => {
   return (
     <Container>
-      <StyledText>알림이 없습니다!</StyledText>
+      <AlertBar alert={"이화 님이 내 글에 좋아요를 남겼습니다."} />
     </Container>
   );
 };
