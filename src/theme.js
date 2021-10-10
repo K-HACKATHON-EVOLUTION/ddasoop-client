8dff9cbbb4394319e5be48b28c88a796db632c11
Haerin Lee <dazzlynnnn@gmail.com>
2021-10-10 23:05:06 +0900

    Update colors

--- src/theme.js
+++ src/theme.js
@@ -4,12 +4,9 @@ const colors = {
     grey_0: '#e4e4e4',
     grey_1: '#848484',
     red: '#e84118',
-    blue: '#3679fe',
     color_1: '#FFF5DB',
     color_2: '#9CC27E',
     color_3: '#8CAF71',
-    color_4: '#C4F065',
-    color_5: '#E6E460',
 };
 
 export const theme = {
@@ -32,7 +29,6 @@ export const theme = {
     inputBorder: colors.grey_1,
     headerTintColor: colors.color_3,
     focus: colors.green_3,
-
 
     spinnerBackground: colors.black,
     spinnerIndicator: colors.white,
