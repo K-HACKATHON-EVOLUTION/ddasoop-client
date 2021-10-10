57dca1be5839da8da387792f251f0dd411db9616
Haerin Lee <dazzlynnnn@gmail.com>
2021-10-10 23:14:57 +0900

    Update Place component style

--- src/components/Place.js
+++ src/components/Place.js
@@ -1,28 +1,29 @@
 import React from "react";
 import styled from "styled-components/native";
-import { View } from "react-native";
+import { View, Image } from "react-native";
 
 const Container = styled.View`
-    height: 80px;
+    height: 60px;
     width: 370px;
     display: flex;
     align-items: flex-start;
-    background-color: #e0e0e0;
-    margin: 10px;
-    padding-left: 30px;
+    flex-direction: row;
+    margin-left: 40px;
+    margin-bottom: 30px;
 `;
 
 const Date = styled.Text`
-    font-size: 16px;
-    margin-bottom: 10px;
+    font-size: 15px;
+    margin-bottom: 20px;
+    margin-left: 10px;
 `;
 
 const Place = ({ start, stop }) => {
     return (
         <Container>
+                <Image source={require("../../assets/arrow.png")}></Image>
                 <View>
                     <Date>{start}</Date>
-                    <Date>|</Date>
                     <Date>{stop}</Date>
                 </View>
         </Container>
