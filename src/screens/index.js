59b5bfa2d689c671fb6f7a48ede5330177708294
Haerin Lee <dazzlynnnn@gmail.com>
2021-10-10 23:08:47 +0900

    Add new screens to index.js

--- src/screens/index.js
+++ src/screens/index.js
@@ -9,5 +9,31 @@ import CourseMain from "./CourseMain";
 import Tree from "./Tree";
 import Log from "./Log";
 import LogList from "./LogList";
+import BadgeList from "./BadgeList";
+import Start from "./Start";
+import CourseList from './CourseList';
+import Course from "./Course";
+import GroupSetting from "./GroupSetting";
+import CreateGroup from "./CreateGroup";
+import MemberSetting from "./MemberSetting";
 
-export { Login, Signup, Home, GroupList, Group, Alert, Setting, CourseMain, Tree, Log, LogList };
+export {
+    Login,
+    Signup,
+    Home,
+    GroupList,
+    Group,
+    Alert,
+    Setting,
+    CourseMain,
+    Tree,
+    Log,
+    LogList,
+    BadgeList,
+    Start,
+    CourseList,
+    Course,
+    GroupSetting,
+    CreateGroup,
+    MemberSetting
+};
