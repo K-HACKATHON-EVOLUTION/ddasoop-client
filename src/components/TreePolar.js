c7233346897bfc7df2dccdd669d0efde5ca0affa
Haerin Lee <dazzlynnnn@gmail.com>
2021-10-10 23:04:05 +0900

    Add TreePolar component

---
+++ src/components/TreePolar.js
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components/native";
+import PropTypes from "prop-types";
+import { AntDesign } from "@expo/vector-icons";
+import { Image, View, TouchableOpacity, StyleSheet } from "react-native";
+
+const Outer = styled.View`
+    height: 180px;
+    width: 150px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #f0f0f0;
+    margin: 10px;
+`;
+
+const Inner = styled.View`
+    height: 130px;
+    width: 130px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    background-color: #ffffff;
+`;
+
+const Date = styled.Text`
+    font-size: 14px;
+    color: black;
+    margin-top: 10px;
+`;
+
+const TreePolar = ({ name, uri }) => {
+    return (
+        <Outer>
+            <Inner>
+                <Image style={styles.tree} source={require("../../assets/maintree.png")} />
+            </Inner>
+            <Date>{name}</Date>
+        </Outer>
+    );
+    
+};
+
+const styles = StyleSheet.create({
+    tree: {
+        height: 80,
+        width: 80,
+        resizeMode: "contain"
+    }
+})
+
+export default TreePolar;
