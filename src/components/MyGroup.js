0d711a51fdd90e07d9606d68c4a6628fb3542854
Haerin Lee <dazzlynnnn@gmail.com>
2021-10-27 22:23:35 +0900

    Add Icon to button

--- src/components/MyGroup.js
+++ src/components/MyGroup.js
@@ -4,6 +4,8 @@ import PropTypes from "prop-types";
 import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
 import { UserContext } from "../contexts/User";
 import { useNavigation } from '@react-navigation/native';
+import { AntDesign, Feather } from "@expo/vector-icons";
+
 
 const Container = styled.ImageBackground`
   height: 120px;
@@ -17,18 +19,19 @@ const Container = styled.ImageBackground`
 `;
 
 const MyGroup = ({ name, img, onPress }) => {
-  const navigation = useNavigation(); 
+  const navigation = useNavigation();
   return (
     <View>
       {name ? (
         <TouchableOpacity onPress={() => onPress()}>
-          <Container source={{ uri: `${img}`}}>
-              <Text style={{ fontSize: 17 }}>{name}</Text>
+          <Container source={{ uri: `${img}` }}>
+            <Text style={{ fontSize: 17 }}>{name}</Text>
           </Container>
         </TouchableOpacity>
       ) : (
         <TouchableOpacity onPress={() => navigation.navigate("CreateGroup")}>
           <Container>
+            <AntDesign name="pluscircleo" size={20} style={{ marginBottom: 10 }} />
             <Text style={{ fontSize: 17 }}>숲 만들기</Text>
           </Container>
         </TouchableOpacity>
